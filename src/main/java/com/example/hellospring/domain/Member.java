78bdd3d342b7c6833fd816e83a733dc7adbdaf41
soyeonkim0227 <soyeonkim0227@naver.com>
2022-07-12 23:28:17 +0900

    Create young han T

--- src/main/java/com/example/hellospring/domain/Member.java
+++ src/main/java/com/example/hellospring/domain/Member.java
@@ -1,2 +1,20 @@
-package com.example.hellospring.domain;public class Member {
+package com.example.hellospring.domain;
+
+public class Member {
+
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/com/example/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/example/hellospring/repository/MemoryMemberRepository.java
@@ -1,2 +1,36 @@
-package com.example.hellospring.repository;public class MemoryMemberRepository {
+package com.example.hellospring.repository;
+
+import com.example.hellospring.domain.Member;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static final Map<Long, Member> store = new HashMap<>();
+    private static long sequence = OL;
+
+    @Override
+    public Member save (Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
 }
